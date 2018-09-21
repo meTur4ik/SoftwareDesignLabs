577b75d23bc808604e3e7c0b5ff47686c608baaf
ololoev <mettur4ik@gmail.com>
2018-09-21 11:49:27 +0300

    upgrade project to api version 23

---
+++ app/src/main/java/com/example/asus_user/labs/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.asus_user.labs;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei;
+        try {
+            imei = tm.getDeviceId();
+        }
+        catch (SecurityException e){
+            throw e;
+        }
+        String asd;
+        TextView central = (TextView)findViewById(R.id.textView);
+        central.setText(imei);
+    }
+}
