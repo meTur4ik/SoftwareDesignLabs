4b9bf8e25450f56edc9809bd6a39d6725ef792f6
ololoev <mettur4ik@gmail.com>
2018-09-21 15:34:54 +0300

    made a gradle script for semver and config file for it. also add -dev screen blocking option

--- app/src/main/java/com/example/asus_user/labs/MainActivity.java
+++ app/src/main/java/com/example/asus_user/labs/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.asus_user.labs;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.media.VolumeShaper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -13,15 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         String imei;
         try {
             imei = tm.getDeviceId();
+            TextView central = (TextView)findViewById(R.id.textView);
+            central.setText(imei);
         }
         catch (SecurityException e){
             throw e;
         }
-        TextView central = (TextView)findViewById(R.id.textView);
-        central.setText(imei);
+//        TextView central = (TextView)findViewById(R.id.textView);
+//        central.setText(imei);
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+
+            if(version.contains("-dev")) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            TextView upper = findViewById(R.id.textView2);
+            upper.setText(version);
+        } catch (PackageManager.NameNotFoundException e) {
+            TextView upper = findViewById(R.id.textView2);
+            upper.setText(e.getMessage());
+        }
     }
 }
