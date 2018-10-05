750c07e46ca46aa764f0fc1fc0119896ecbedd71
ololoev <mettur4ik@gmail.com>
2018-10-05 22:36:46 +0300

    add 2-nd manifest to control rotation switch

--- app/src/main/java/com/example/asus_user/labs/MainActivity.java
+++ app/src/main/java/com/example/asus_user/labs/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.asus_user.labs;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -17,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    0);
+        }
+
 
 
         TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
@@ -27,9 +39,9 @@ public class MainActivity extends AppCompatActivity {
             central.setText(imei);
         }
         catch (SecurityException e){
-            throw e;
+            this.finish();
         }
-
+//
         try {
             PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
             String version = pInfo.versionName;
